b1c19ec503228864e030d77beff8b8aea46b1309
laugardc <laugardc@gmail.com>
2023-03-17 15:03:02 -0600

    pizza lover login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -5,15 +5,15 @@ const Login = () => {
     return (
         <div className="login">
             <div className="form-container">
-                <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
-                <h1 className="title">Login</h1>
+                <img src="../images/718d36eb-f514-4e46-8e16-608ec5fca243.png" alt="logo" className="logo" />
+                <h1 className="title">Welcome to Pizza Lover</h1>
                 <p className="subtitle">Enter your user and password</p>
                 <form action="/" className="form">
                     <label for="user" className="label">User</label>
                     <input type="user" id="user" placeholder="Your name" className="input input-user" />
                     <label for="password" className="label">Password</label>
                     <input type="password" id="password" placeholder="*********" className="input input-password" />
-                    <input type="submit" value="Confirm" className="primary-button login-button" />
+                    <input type="submit" value="Sign in" className="primary-button login-button" />
                 </form>
             </div>
         </div>
