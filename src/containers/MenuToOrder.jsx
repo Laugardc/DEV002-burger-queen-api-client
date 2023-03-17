c1752b97e9a0ca4e78bf6edbf344c43eab116e91
laugardc <laugardc@gmail.com>
2023-03-17 17:39:57 -0600

    Router funcionando home/login/menu-to-order/not-fo

---
+++ src/containers/MenuToOrder.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/MenuToOrder.scss';
+
+const Products = () => {
+    return (
+        <h1 className="products">Products Pizza Lover</h1>
+    );
+}
+
+export default Products;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import createRoot from 'react-dom';
+import App from './routes/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+createRoot.render(<App />, document.getElementById('app'));
